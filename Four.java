1bdc9ea24d58aa0c90429f4ecf611de2f0361cc6
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-05-03 15:51:54 +0530

    java

---
+++ Four.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Four
+{
+public static void main(String ar[])
+    {
+// int c=2;
+// int d=3;
+
+// System.out.println(c&d);
+// System.out.println(c|d);
+// System.out.println(c^d);
+
+//next
+
+// int c=7;
+// int d=3;
+
+// System.out.println(c&d);//3
+// System.out.println(c|d);//7
+// System.out.println(c^d);//4
+
+int number=24;
+// String str = (number%2==0)?"even":"odd";
+// System.out.println(str);
+
+// int result = (number>10 && number<20)?number:0;
+// System.out.println(result);
+
+// System.out.println((number>10 && number<20)?number:"ii");
+
+//  System.out.println(((number>10 && number<20) && (number%2==0 && number%3==0)) ? number:"ii");
+
+System.out.println
+((number>20 && number<30 ) ? ((number%2==0 && number%3==0)?"yes" : "no"):"invalid");
+    }
+    }
