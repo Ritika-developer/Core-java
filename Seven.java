3a60b7ce5345c90545f998bb395e1c331e90dcb6
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-05-06 15:44:08 +0530

    java

---
+++ Seven.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Seven
+{
+public static void main(String ar[])
+    {
+            //java mai if kai nader inisialization nahi kar sakte yaha c++ mai kar sakte h 
+            //java mai if(true)
+            //c++ mai if(a=1)
+            //java.util=package
+            //scanner= class
+//in case of object the address of object is compired by the compairision opreator but in case of variables there value are compaire
+          //string :- pool,object
+
+
+            // Scanner obj = new Scanner (System.in);
+            // char input;
+            // System.out.println("enter a char");
+            // input = obj.next().charAt(0);
+            // if(input>='A' && input<='Z'){
+            //     System.out.println("uppercase");
+            // }
+            // else if(input>='a' && input<='z'){
+            //     System.out.println("lowercase");
+            // }
+          
+            // else{
+            //     System.out.println("Invalid");
+            // }
+            Scanner obj = new Scanner (System.in);
+            System.out.println("enter");
+            String name = obj.next();
+            if(name.equals("ritika")){
+                System.out.println("true");
+            }
+            else{
+                System.out.println("false");
+            }
+    }
+    }
