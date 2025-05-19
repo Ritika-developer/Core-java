faf6e6ac7d695198f7ebcb5a331afdab809d8c95
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-05-19 18:19:09 +0530

    java

--- Loops.java
+++ Loops.java
@@ -54,19 +54,120 @@ public class Loops {
 
 
 //find lcm
-int num1 =12;
-int num2 =18;
-int mx = (num1>num2)?num1:num2;
-int i=mx;
-while(true){
-if(i%num1==0 && i%num2==0){
-    break;
+// int num1 =12;
+// int num2 =18;
+// int mx = (num1>num2)?num1:num2;
+// int i=mx;
+// while(true){
+// if(i%num1==0 && i%num2==0){
+//     break;
+// }
+//     i=i+mx;
+//     }
+//         System.out.println(i);
+
+
+
+
+//largest digit 
+
+// int number = 1234;
+// int res = 0;
+// while(number!=0){
+//     int digit = number%10;
+//     if (res<digit)
+//         res=digit;
+//     System.out.println(digit);
+//     number = number/10;
+// }
+//     System.out.print(res);
+
+
+
+//reverse
+// int number = 1234;
+// int rev = 0;
+// while(number!=0){
+//     int digit = number%10;
+//     rev = rev*10+digit;
+//     number = number/10;
+// }
+//     System.out.print(rev);
+
+
+
+//prime number
+// int number = 12;
+// int counter  =0;
+// for (int i=1;i<=11;i++){
+//     if (number%i==0){
+// counter++;
+//     }
+// }
+// if (counter==2){
+//     System.out.print("prime number");
+// }
+// else{
+//         System.out.print(" not a prime number");
+// }
+
+
+
+//amstrong
+// int number = 153;
+// int org = number;
+// int sum = 0;
+// while(number!=0){
+//   int  digit =number%10;
+//   sum = sum+digit*digit*digit;
+//   number = number /10;
+// }
+// if(sum==org)
+// System.out.print("arms");
+// else{
+//     System.out.print(" not arms");
+// }
+
+
+
+
+// int number = 153;
+// int org = number;
+// int sum = 0;
+// int counter = 0;
+// while(number!=0){
+//   int  digit =number%10;
+//   counter++;
+//   sum = sum+digit*digit*digit;
+//   number = number /10;
+// }
+//   System.out.print(counter);
+
+// if(sum==org)
+// System.out.print("arms");
+// else{
+//     System.out.print(" not arms");
+// }
+
+
+
+
+//palindrom
+
+int number = 121;
+int palindrom = number;
+int rev = 0;
+while(number!=0){
+    int digit = number%10;
+    rev = rev*10+digit;
+    number = number/10;
 }
-    i=i+mx;
-    }
-        System.out.println(i);
+if (palindrom==rev)
+    System.out.print("palindrom");
+else{
+        System.out.print(" not palindrom");
 
-
+}
 
 
 
