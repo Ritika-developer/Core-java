77d4d31ffa63a1255d7a4147b7be683b94f270c3
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-05-20 20:49:59 +0530

    java

--- Loops.java
+++ Loops.java
@@ -194,5 +194,68 @@ public class Loops {
 
 
 
+//gp 
+// nth = a*r^(n-1);
+// a=first term 
+// r = common ratio
+// n=5
+
+// int a =1;
+// int n=6;
+// int r=3;
+// double nterm=0;
+// for (int i=a;i<=n;i++){
+//    nterm =  a*Math.pow(r,(n-1));
+// }
+//    System.out.println(nterm);
+
+
+// int a =1;
+// int n=6;
+// int r=3;
+// double nterm=0;
+// for (int i=a;i<=n;i++){
+//    nterm =  a*Math.pow(r,i);
+//       System.out.println(nterm);
+// }
+
+
+// int a =1;
+// int n=6;
+// int r=3;
+// for (int i=0;i<n;i++){
+//       System.out.println(a);
+//       a=r*a;
+// }
+
+
+
+//fabonacci series
+int i=1;
+int n=5;
+ int first = 1;
+ int second = 2;
+while(i<=n)
+{
+ if(i==1){
+    System.out.println(i);
+ i++;
+ continue;
+ }
+ if(i==2){
+      System.out.println(i);
+ i++;
+continue;
+ }
+ int next = first+second;
+ first=second;
+ second=next;
+ i++;
+System.out.println(next);
+}
+
+
+
+
     }
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,37 @@
-  yield vegResult;
+
+// int a =1;
+// int n=5;
+// int d=2;
+// int nterm=0;
+// for (int i=a;i<=n;i++){
+//    nterm =  a+(n-1)*d;
+// }
+//    System.out.println(nterm);
+
+
+
+
+//gp 
+// nth = a*r^(n-1);
+// a=first term 
+// r = common ratio
+// n=5
+
+// int a =1;
+// int n=6;
+// int r=3;
+// double nterm=0;
+// for (int i=a;i<=n;i++){
+//    nterm =  a*Math.pow(r,(n-1));
+// }
+//    System.out.println(nterm);
+
+
+// int a =1;
+// int n=6;
+// int r=3;
+// double nterm=0;
+// for (int i=a;i<=n;i++){
+//    nterm =  a*Math.pow(r,i);
+//       System.out.println(nterm);
+// }
