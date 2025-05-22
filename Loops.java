c6e50fa5f922442f5e6f4f7ad6ce2e537402add7
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-05-22 18:05:36 +0530

    java

--- Loops.java
+++ Loops.java
@@ -322,8 +322,8 @@ public class Loops {
 
 //pattern
  for(int r=1;r<=5;++r){
-    for(int c=1;c<=5;++c){
-        if()
+    for(int c=1;c<=9;++c){
+        if(c>=6-r && c<=4+r)
         {
             System.out.print("*");
         }
