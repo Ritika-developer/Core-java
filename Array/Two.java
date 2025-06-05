e021037b37717e3c203356508577f4791b8da8ab
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-06-05 18:10:50 +0530

    java

--- Array/Two.java
+++ Array/Two.java
@@ -94,21 +94,43 @@ public static void main (String ar[]){
 // }
 
 
+
+//2 way
+// int arr[] = {1,4,2,5,6,3};
+// int n = arr.length;
+// for(int i = 0 ; i<n-1 ;i++){
+//     for(int j = i+1 ;j<n ;j++){
+//         if(arr[i]>arr[j]){
+//         int temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//         }
+//     }
+//     System.out.print(arr[i]);
+// }
+
+
 //sir ka logic
-int arr[] = {1,4,2,5,6,3};
-int n = arr.length;
-for(int i = 0 ; i<n-1 ;i++){
-    for(int j = 0 ;j<n-1-i ;j++){
-        if(arr[j]>arr[j+1]){
-        int temp = arr[j+1];
-        arr[j+1] = arr[j];
-        arr[j] = temp;
-        }
-    }
-}
-for(int i=0 ; i<n ; i++){
-    System.out.print(arr[i]); 
-}
+// int arr[] = {1,4,2,5,6,3};
+// int n = arr.length;
+// for(int i = 0 ; i<n-1 ;i++){
+//     for(int j = 0 ;j<n-1-i ;j++){
+//         if(arr[j]>arr[j+1]){
+
+//         int temp = arr[j];
+//         arr[j] = arr[j+1];
+//         arr[j+1] = temp;
+//         }
+//     }
+// }
+// System.out.print("reverse array is = ");
+// for(int i=0 ; i<n ; i++){
+//       System.out.print(arr[i] + " "); 
+// }
+
+
+
+
 
 //desending
 // int arr[] = {1,4,2,5,6,3};
@@ -126,6 +148,43 @@ for(int i=0 ; i<n ; i++){
 // }
 
 
+
+
+
+//minimum value
+// int arr[] = {1,4,2,5,6,3};
+// int n = arr.length;
+// for(int i = 0 ; i<n-1 ;i++){
+//     for(int j = i+1 ;j<n ;j++){
+//         if(arr[i]>arr[j]){
+//         int temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//         }
+//     }
+// }
+
+// for(int i=0;i<1;i++){
+//         System.out.print(arr[i]);
+// }
+
+
+//maximum value
+int arr[] = {1,4,2,5,6,3};
+int n = arr.length;
+for(int i = 0 ; i<n-1 ;i++){
+    for(int j = i+1 ;j<n ;j++){
+        if(arr[i]<arr[j]){
+        int temp = arr[i];
+        arr[i] = arr[j];              
+        arr[j] = temp;
+        }
+    }
+}
+
+for(int i=0;i<1;i++){
+        System.out.print(arr[i]);
+}
 
 }
     }
