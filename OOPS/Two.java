6a850e167b9c6df373315f30da45f59af8ae64cc
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-06-24 18:22:33 +0530

    24-06-25

--- OOPS/Two.java
+++ OOPS/Two.java
@@ -1,10 +1,28 @@
+// class Two{
+//     int a;
+//     Two(){
+//         a=10;
+//     }
+//     public static void main(String args[]){
+//         Two obj = new Two();
+//         System.out.print(obj.a);
+//     }
+// } 
+
+
+
 class Two{
-    int a;
+    String a;
+    int b;
+    //default
     Two(){
-        a=10;
+       System.out.print("i am default constructor");
+    }
+//overloading of constructor
+    Two(int a){
+       System.out.print("i am overloaded constructor");
     }
     public static void main(String args[]){
-        Two obj = new Two();
-        System.out.print(obj.a);
+        Two obj = new Two(10);//parameter
     }
-}
+} 
